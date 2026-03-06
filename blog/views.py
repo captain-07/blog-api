a0a1b8368586d0d1cc526fdb41e292847a59f562
Debjyoti <sahadebjyoti363@gmail.com>
2026-03-06 21:15:34 +0530

    Applied custom permission classes

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from .models import Post
 from rest_framework import viewsets, filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .serializers import PostSerializer
+from .permissions import IsAdminOrReadOnly
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -11,7 +12,7 @@ class PostViewSet(viewsets.ModelViewSet):
     )
 
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAdminOrReadOnly]
 
     filter_backends = [
         filters.SearchFilter,
